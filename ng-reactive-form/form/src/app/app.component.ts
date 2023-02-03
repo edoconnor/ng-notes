94ec061eee5b075e7361bbca46414e7f6df2044d
Ed O'Connor <ed@oconnor.app>
2023-02-03 17:43:29 -0500

    reactive forms

---
+++ ng-reactive-form/form/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+ 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  registerForm = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+  });
+
+  isSubmitted = false;
+
+  constructor(private fb: FormBuilder){}
+
+  onSubmit(): void {
+    console.log('submitted form', 
+    this.registerForm.value,
+    this.registerForm.invalid
+    );
+    this.isSubmitted = true
+  }
+}
