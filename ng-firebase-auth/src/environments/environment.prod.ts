b05915d40ff3066831dbc8e881415e5bda421f6b
Ed O'Connor <ed@oconnor.app>
2023-03-28 10:22:20 -0400

    firebase auth

---
+++ ng-firebase-auth/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'ng-blog-x',
+    appId: '1:616651828055:web:977f0eed859ccfffa1ac12',
+    storageBucket: 'ng-blog-x.appspot.com',
+    apiKey: 'AIzaSyCo77OazfDPPVc3wudfIEMcWkzlmenfh_A',
+    authDomain: 'ng-blog-x.firebaseapp.com',
+    messagingSenderId: '616651828055',
+  },
+  production: true
+};
